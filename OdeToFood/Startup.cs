2b025fa7c43c9f80de50b72431597c81afe23528
janokularczyk <janekokularczyk@gmail.com>
2022-09-13 14:30:52 +0200

    setuping for endpoints

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -30,6 +30,7 @@ namespace OdeToFood
             services.AddScoped<IRestaurantData, SqlRestaurantData>();
 
             services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -56,6 +57,7 @@ namespace OdeToFood
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
